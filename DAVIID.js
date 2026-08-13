1e9ea3cfd5fa4a7c99e58b94f9deeb7b60cdb17f
Chimapro123 <chiboyeze73@gmail.com>
2026-08-13 15:01:07 +0100

    udated version

--- DAVIID.js
+++ DAVIID.js
@@ -5,24 +5,24 @@ document.querySelector(".dark").addEventListener("click", () => {
   document.querySelector("body").classList.toggle("dark-mode");
 });
 
-let name = prompt("enter your name: ");
-let age = prompt("enter your age: ");
+// let name = prompt("enter your name: ");
+// let age = prompt("enter your age: ");
 
-if (name) {
-  console.log(name);
-}
-if (age < 18) {
-  console.log("user is too younge");
-  console.log = "enter your age: ";
-} else {
-  console.log("you pass");
-}
-const age = age;
-name = "name";
-console.log(`Welcome to Aptech ${name}`);
+// if (name) {
+//   console.log(name);
+// }
+// if (age < 18) {
+//   console.log("user is too younge");
+//   console.log = "enter your age: ";
+// } else {
+//   console.log("you pass");
+// }
+// const age = age;
+// name = "name";
+// console.log(`Welcome to Aptech ${name}`);
 
-let geolocation = window.navigator.geolocation.getCurrentPosition((p) => {
-  console.log(p.coords.latitude, p.coords.longitude, p.timestamp.toString());
-});
+// let geolocation = window.navigator.geolocation.getCurrentPosition((p) => {
+//   console.log(p.coords.latitude, p.coords.longitude, p.timestamp.toString());
+// });
 
 // console.log("hello");
