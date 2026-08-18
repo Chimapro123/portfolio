107cb9bd8165264f55e4c4cb97c12b5bb3963e55
Chimapro123 <chiboyeze73@gmail.com>
2026-08-18 22:14:10 +0100

    Updated version of my Portfolio!

--- DAVIID.js
+++ DAVIID.js
@@ -1,28 +1,6 @@
-document.querySelector(".right-text").addEventListener("mouseover", () => {
-  document.querySelector("body").style.color = "rgb(236, 238, 167)";
-});
 document.querySelector(".dark").addEventListener("click", () => {
   document.querySelector("body").classList.toggle("dark-mode");
 });
-
-// let name = prompt("enter your name: ");
-// let age = prompt("enter your age: ");
-
-// if (name) {
-//   console.log(name);
-// }
-// if (age < 18) {
-//   console.log("user is too younge");
-//   console.log = "enter your age: ";
-// } else {
-//   console.log("you pass");
-// }
-// const age = age;
-// name = "name";
-// console.log(`Welcome to Aptech ${name}`);
-
-// let geolocation = window.navigator.geolocation.getCurrentPosition((p) => {
-//   console.log(p.coords.latitude, p.coords.longitude, p.timestamp.toString());
-// });
-
-// console.log("hello");
+console.log(
+  "Hello, Employer! Take your time to explore my portfolio. I hope you enjoy exploring my work and discovering what I can bring to your team. Thank you for visiting my portfolio website. I look forward to the opportunity to work with you and contribute to your team's success.",
+);
